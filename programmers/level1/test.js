c59f89c3ca68ebfd72405abe5b17afa4444439da
ssups <shalstlqruki@gmail.com>
2022-10-11 17:59:24 +0900

    1011

--- programmers/level1/test.js
+++ programmers/level1/test.js
@@ -1,6 +1,35 @@
-const str = "hihihih23df";
-const temp = str.match("hi");
-console.log(str);
-console.log(temp);
+function Card() {
+  this.attack = Math.floor(Math.random() * 45) + 1;
+  this.def = Math.floor(Math.random() * 45) + 1;
+  this.average = (this.attack + this.def) / 2;
+  this.rarity =
+    this.average >= 40 ? "UltraRare" : this.average < 40 && this.average >= 20 ? "Rare" : "Normal";
+}
 
-console.log(0 + "0" * 1);
+const cards = Array(5)
+  .fill(0)
+  .map(el => new Card());
+
+console.log(cards);
+
+function Upgrade(card, attribute, amount) {
+  card[attribute] += amount;
+  card["average"] = (card["attack"] + card["def"]) / 2;
+  card["rarity"] =
+    card["average"] >= 40
+      ? "UltraRare"
+      : card["average"] < 40 && card["average"] >= 20
+      ? "Rare"
+      : "Normal";
+  return card;
+}
+
+cards[0] = Upgrade(cards[0], "attack", 20);
+
+console.log(cards);
+
+cards.push(new Card());
+
+console.log(cards);
+
+export { Card, Upgrade };
--- programmers/최소공배수 최대공약수(유클리드호재법).js
+++ programmers/최소공배수 최대공약수(유클리드호재법).js
@@ -1,19 +1,19 @@
 // 내 답:
 function func(num) {
-    let result = [];
-    for (let i = 0; i <= num; i++) {
-        if (num % i === 0) result.push(i);
-    }
-    return result;
+  let result = [];
+  for (let i = 0; i <= num; i++) {
+    if (num % i === 0) result.push(i);
+  }
+  return result;
 }
 function solution(n, m) {
-    const temp = Math.max(...func(n).filter(el => func(m).includes(el)));
-    return [temp, temp * (n / temp) * (m / temp)];
+  const temp = Math.max(...func(n).filter(el => func(m).includes(el)));
+  return [temp, temp * (n / temp) * (m / temp)];
 }
 
 // 좋은 답:
 function gcdlcm(a, b) {
-    var r;
-    for (var ab = a * b; (r = a % b); a = b, b = r) {}
-    return [b, ab / b];
+  var r;
+  for (var ab = a * b; (r = a % b); a = b, b = r) {}
+  return [b, ab / b];
 }
